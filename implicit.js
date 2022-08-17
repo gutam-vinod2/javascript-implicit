e9ca4223fde6c4b39e8444f9b60dcedd9df4a8b3
[firstname lastname] <[valid-email]>
2022-08-17 16:38:53 +0530

    imlicit.js

---
+++ implicit.js
@@ -0,0 +1,24 @@
+var a =12 + "" 
+var a = "15" *2
+var b = "15" - "11"
+var c = undefined + 6
+var c = "hello" +null 
+var a = null + 25
+var a = true +true
+var a = false + 10
+var a = 10 * [6]
+var a =  10 * [10,20]
+var a = [1] + [1,2]
+
+
+console.log(a);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(c);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
+console.log(a);
